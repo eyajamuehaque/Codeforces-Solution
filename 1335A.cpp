054fdd8875034e3ea59adbdfb6e2466bbb806654
Eya Jamue Haque <147173274+eyajamuehaque@users.noreply.github.com>
2024-07-13 17:53:16 +0600

    Create 1335A.cpp

---
+++ 1335A.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+int n;
+cin>>n;
+
+long long a,b;
+
+for(int i=0;i<n;i++){
+    cin>>a;
+    b=(a-1)/2;
+    cout<<b<<endl;
+}
+
+
+return 0;
+}
