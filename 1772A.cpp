8da5025ea4f956e8d8becdf11aed8b8f760d858f
Eya Jamue Haque <147173274+eyajamuehaque@users.noreply.github.com>
2024-08-22 11:58:56 +0600

    1772A - A+B?

---
+++ 1772A.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+int t;
+cin>>t;
+
+while(t--){
+
+    int a,b;
+    char s;
+    cin>>a>>s>>b;
+    if(s=='+'){
+        cout<<a+b<<endl;
+    }
+}
+
+
+return 0;
+}
