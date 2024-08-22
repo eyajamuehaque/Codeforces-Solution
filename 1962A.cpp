8d4abd48a12768a4769dc5e0348324adc5f2727a
Eya Jamue Haque <147173274+eyajamuehaque@users.noreply.github.com>
2024-08-22 13:22:38 +0600

    1926A - Vlad and the Best of Five

---
+++ 1962A.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+int t;
+cin>>t;
+while(t--){
+    string s;
+    cin>>s;
+    int a=0,b=0;
+    for(int i=0;i<5;i++){
+        if(s[i]=='A'){
+            a++;
+        }else if(s[i]=='B'){
+            b++;
+        }
+    }
+    if(a>b){
+        cout<< "A"<<endl;
+    }else{
+        cout<< "B"<<endl;
+    }
+}
+
+
+return 0;
+}
