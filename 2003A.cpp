f7d0f7c6088252496bd7baed83816f04404581ec
Eya Jamue Haque <147173274+eyajamuehaque@users.noreply.github.com>
2024-09-20 10:31:00 +0600

    2003A - Turtle and Good Strings

---
+++ 2003A.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+int t;
+cin>>t;
+
+while(t--){
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    if(s[0]==s[n-1]){
+        cout<< "NO"<<endl;
+    }else{
+        cout<< "YES"<<endl;
+    }
+
+}
+
+return 0;
+}
